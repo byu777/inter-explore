c9aaf00e969194b2bbe0c80ce376510feedf0db0
kyulyeon <65508439+kyulyeon@users.noreply.github.com>
2022-05-02 17:50:09 -0700

    connected to Mongodb

---
+++ router/router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoUri = 'mongodb+srv://user:123@cluster0.1ozdh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, 
+    async(err)=>{
+        if(err) throw err;
+        console.log("conncted to db")
+    
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('errors with connecting to mongoDB',err)
+});
+
+app.get('/',(req,res) => {
+res.send('Hi there!');
+});
+
+app.listen(3000,()=>{
+console.log('Listening on port 3000');
+});
