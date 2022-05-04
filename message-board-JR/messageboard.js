fa3426da47ded88f6cf464631137f15160257296
Johnny Ryue <johnryue1993@gmail.com>
2022-05-04 11:14:41 -0700

    created UI for Chatroom and Create-Event pages; updated UI for Message board page

--- message-board-JR/messageboard.js
+++ message-board-JR/messageboard.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  Image,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { Alert } from "react-native-web";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,21 +21,36 @@ const MessageBoardPage = () => {
         {/* make as many screens as you want; order doesnt matter */}
         <Stack.Screen
           name="MessageBoard"
-          component={MessageBoardScreen}
+          component={MessageBoardLayout}
           options={{ title: "Inbox" }}
         />
 
         {/* headerTitleAlign -> center the title */}
-        <Stack.Screen style={cont_styles.text_center}
+        <Stack.Screen
+          style={mb_styles.text_center}
           name="Chatrooms"
           component={ChatroomScreen}
-          options={{ title: "Jim" , headerTitleAlign: "center",}}
+          // ********* is it better to create event from the Stack using alert or is there a dialog box equivalent?   **************
+          // **********************************************************************
+          options={{
+            title: "Basketball", //rename this to a group-name or name of person if 1:1 chat
+            headerTitleAlign: "center",
+          }}
         />
 
         <Stack.Screen
           name="EventBoard"
-          component={EventScreen}
+          component={EventBoard}
           options={{ title: "Your Events" }}
+        />
+
+        <Stack.Screen
+          name="MakeEvent"
+          component={MakeEventPage}
+          options={{ 
+            title: "Create an Event",
+            headerTitleAlign: "center",
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -34,28 +58,134 @@ const MessageBoardPage = () => {
 };
 
 // ---------------------------Message Board page ------------------------------
-const MessageBoardScreen = ({ navigation }) => {
+// const MessageBoardScreen = ({ navigation }) => {
+//   const [text, onChangeText] = React.useState("");
+//   return (
+//     <View style={mb_styles.container}>
+//       <View style={mb_styles.textinput_cont}>
+//         <TextInput
+//           style={mb_styles.textinput_search}
+//           onChangeText={onChangeText}
+//           placeholder="search.."
+//           value={text} //allows alphanumeric input into TextInput
+//         />
+//       </View>
+
+//       <View style={mb_styles.button_chat}>
+//         <Button
+//           title="Go"
+//           onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+//         />
+//       </View>
+
+//       {/* Created a custom component, and nest it inside the 'MAIN' View */}
+//       <MessageBoardLayout></MessageBoardLayout>
+//     </View>
+//   );
+// };
+
+// --------------------------- Message Board page -----------------------------
+const MessageBoardLayout = ({ navigation }) => {
   const [text, onChangeText] = React.useState("");
   return (
-    <View style={cont_styles.container}>
-      <View style={cont_styles.textinput_cont}>
+    <View>
+      <View style={mb_styles.textinput_cont}>
         <TextInput
-          style={cont_styles.textinput_search}
+          style={mb_styles.textinput_search}
           onChangeText={onChangeText}
-          placeholder="useless placeholder"
+          placeholder="search.."
           value={text} //allows alphanumeric input into TextInput
         />
       </View>
 
-      <View style={cont_styles.button_chat}>
-        <Button
-          title="Go"
-          onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
-        />
-      </View>
+      {/* inbox container */}
+      <View style={mb_styles.mb_chat_container}>
+        <View style={mb_styles.list_item_inbox}>
+          <View style={mb_styles.mb_image_container}>
+            <Image
+              style={mb_styles.mb_image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+            <Button
+              title="Go"
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            />
+          </View>
+          <View style={mb_styles.list_item_description}>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+          </View>
+        </View>
 
-      {/* Created a custom component, and nest it inside the 'MAIN' View */}
-      <MessageBoardLayout></MessageBoardLayout>
+        <View style={mb_styles.list_item_inbox}>
+          <View style={mb_styles.mb_image_container}>
+            <Image
+              style={mb_styles.mb_image_container}
+              source={require("./assets/favicon.png")}
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            ></Image>
+            <Button
+              title="Go"
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            />
+          </View>
+          <View style={mb_styles.list_item_description}>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+          </View>
+        </View>
+
+        <View style={mb_styles.list_item_inbox}>
+          <View style={mb_styles.mb_image_container}>
+            <Image
+              style={mb_styles.mb_image_container}
+              source={require("./assets/favicon.png")}
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            ></Image>
+            <Button
+              title="Go"
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            />
+          </View>
+          <View style={mb_styles.list_item_description}>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+          </View>
+        </View>
+
+        <View style={mb_styles.list_item_inbox}>
+          <View style={mb_styles.mb_image_container}>
+            <Image
+              style={mb_styles.mb_image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+            <Button
+              title="Go"
+              onPress={() => navigation.navigate("Chatrooms", { name: "Jane" })}
+            />
+          </View>
+          <View style={mb_styles.list_item_description}>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+            <Text style={{ flexShrink: 1 }}>
+              lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -63,26 +193,245 @@ const MessageBoardScreen = ({ navigation }) => {
 // ---------------------------Chatroom page ------------------------------
 const ChatroomScreen = ({ navigation }) => {
   return (
-    <View style={cont_styles.container}>
-      <Text style={mb_styles.baseText}>
-        I am bold
-        <Text style={mb_styles.innerText}> and red</Text>
-      </Text>
-      <Button
-        title="Go"
-        onPress={() => navigation.navigate("EventBoard", { name: "Jane" })}
-      />
+    // main container
+    <View style={chatroom_styles.main_container}>
+      {/* container for member list */}
+      <View style={chatroom_styles.member_container}>
+        {/* pull names from group collection and populate here */}
+        <Text>Eric, Richard, Justin, Gabriel, Johnny...</Text>
+        <Button
+          title="Members"
+          style={{ padding: 10 }}
+          onPress={() =>
+            Alert.alert("Members", "Members of Basketball", [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel pressed"),
+                style: "cancel",
+              },
+              {
+                text: "OK",
+                onPress: () => console.log("OK pressed"),
+                style: "ok",
+              },
+            ])
+          }
+        />
+      </View>
+
+      {/* container for chat messages area */}
+      <View style={chatroom_styles.chat_area}>
+        <View style={chatroom_styles.chat_row}>
+          <View style={chatroom_styles.image_container}>
+            <Image
+              style={chatroom_styles.image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+          </View>
+          <Text>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </Text>
+        </View>
+
+        <View style={chatroom_styles.chat_row}>
+          <View style={chatroom_styles.image_container}>
+            <Image
+              style={chatroom_styles.image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+          </View>
+          <Text>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </Text>
+        </View>
+
+        <View style={chatroom_styles.chat_row}>
+          <View style={chatroom_styles.image_container}>
+            <Image
+              style={chatroom_styles.image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+          </View>
+          <Text>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </Text>
+        </View>
+
+        <View style={chatroom_styles.chat_row}>
+          <View style={chatroom_styles.image_container}>
+            <Image
+              style={chatroom_styles.image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+          </View>
+          <Text>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </Text>
+        </View>
+
+        <View style={chatroom_styles.chat_row}>
+          <View style={chatroom_styles.image_container}>
+            <Image
+              style={chatroom_styles.image_container}
+              source={require("./assets/favicon.png")}
+            ></Image>
+          </View>
+          <Text>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </Text>
+        </View>
+      </View>
+
+      <View style={chatroom_styles.make_event}>
+        <Button
+          onPress={() => navigation.navigate("MakeEvent", { name: "" })} //add dialog box to input details
+          title="Make Event"
+          color="orange"
+        />
+      </View>
     </View>
   );
 };
 
-// ---------------------------Events page ------------------------------
-const EventScreen = ({ navigation }) => {
+// -------------------------- CREATE AN EVENT PAGE -------------------------------------
+const MakeEventPage = () => {
   return (
-    <View style={cont_styles.container}>
-      <Text style={mb_styles.baseText}>
+    <View style={create_event.event_container}>
+      <View style={create_event.title}>
+      <Text
+          style={{
+            flex: 1,
+            color: "black",
+          }}
+        >
+          Title
+        </Text>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "blue",
+            flex: 2,
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 2,
+            }}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View style={create_event.location}>
+        <Text
+          style={{
+            flex: 1,
+            color: "black",
+          }}
+        >
+          Location
+        </Text>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "blue",
+            flex: 2,
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 2,
+            }}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View style={create_event.date}>
+      <Text
+          style={{
+            flex: 1,
+            color: "black",
+          }}
+        >
+          Date
+        </Text>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "blue",
+            flex: 2,
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 2,
+            }}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View style={create_event.description}>
+      <Text
+          style={{
+            flex: 1,
+            color: "black",
+          }}
+        >
+          Description
+        </Text>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "blue",
+            flex: 9,
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 2,
+            }}
+          ></TextInput>
+        </View>
+      </View>
+
+      <View style={create_event.buttons}>
+        <Button
+          title="Cancel"
+          style={{ padding: 10 }}
+          onPress={() =>
+            Alert.alert("Members", "Members of Basketball", [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel pressed"),
+                style: "cancel",
+              },
+            ])
+          }
+        />
+        <Button
+          title="Schedule"
+          onPress={() =>
+            Alert.alert("Success", "Event was successfully created.", [
+              {
+                text: "OK",
+                onPress: () => console.log("OK pressed"),
+                style: "ok",
+              },
+            ])
+          }
+        />
+      </View>
+    </View>
+  );
+};
+
+// ---------------------------LIST OF EVENTS page ------------------------------
+// havent made yet!!!!!!!!!!!!!!!!!
+const EventBoard = ({ navigation }) => {
+  return (
+    <View style={chatroom_styles.container}>
+      <Text style={chatroom_styles.baseText}>
         I am bold
-        <Text style={mb_styles.innerText}> and red</Text>
+        <Text style={chatroom_styles.innerText}> and red</Text>
       </Text>
 
       <Button
@@ -93,25 +442,10 @@ const EventScreen = ({ navigation }) => {
   );
 };
 
-// --------------------------- flex for MessageBoard -----------------------------
-const MessageBoardLayout = () => (
-  <View>
-    <View style={cont_styles.chat_container}>
-      <View style={cont_styles.box1} />
-
-      <View style={cont_styles.box2} />
-
-      <View style={cont_styles.box3} />
-
-      <View style={cont_styles.box4} />
-    </View>
-  </View>
-);
-
-// ------------------------------- STYLING ---------------------------------
+// ***------------------------------- STYLING ---------------------------------*********************
 
 // style sheet for message board page
-const cont_styles = StyleSheet.create({
+const mb_styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 8,
@@ -126,7 +460,7 @@ const cont_styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "black",
     height: 40,
-    width: 350,
+    width: 300,
     textAlign: "center",
     justifyContent: "center",
     alignSelf: "center",
@@ -134,35 +468,34 @@ const cont_styles = StyleSheet.create({
   },
   textinput_search: {
     flex: 1,
-    fontSize: 24,
+    fontSize: 20,
+    alignSelf: "flex-start",
+    margin: 5,
     flexDirection: "row",
     flexWrap: "wrap",
     textAlign: "center",
-    
   },
-  chat_container: {
+  mb_chat_container: {
     flexDirection: "column",
     justifyContent: "flex-end", //causes the chat container to start from bottom
     height: 600,
-    padding: 20,
+    padding: 10,
+    marginRight: 20,
   },
-  box1: {
-    backgroundColor: "red",
+  mb_image_container: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: 5,
+  },
+  list_item_description: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "center",
     flex: 1,
-    margin: 10,
   },
-  box2: {
-    backgroundColor: "green",
-    flex: 1,
-    margin: 10,
-  },
-  box3: {
-    backgroundColor: "yellow",
-    flex: 1,
-    margin: 10,
-  },
-  box4: {
-    backgroundColor: "purple",
+  list_item_inbox: {
+    flexDirection: "row",
     flex: 1,
     margin: 10,
   },
@@ -176,23 +509,77 @@ const cont_styles = StyleSheet.create({
 });
 
 // style sheet for chatroom page
-const mb_styles = StyleSheet.create({
-  container: {
+const chatroom_styles = StyleSheet.create({
+  main_container: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
-    marginTop: 8,
-    backgroundColor: "orange",
+    height: 500,
   },
-
-
-
-  inboxText: {
-    fontWeight: "bold",
+  member_container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignSelf: "center",
+    alignItems: "center",
     flex: 1,
+    margin: 20,
+    flex: 5,
+    flexWrap: "wrap",
+  },
+  // chat_container: {
+  //   flexDirection: "column",
+  //   flex: 0.2,
+  //   marginTop: 8,
+  //   backgroundColor: "transparent",
+  // },
+  image_container: {
+    flexDirection: "row",
     alignItems: "flex-start",
-    fontSize: 25,
+    justifyContent: "space-around",
   },
-  innerText: {
-    color: "red",
+  chat_area: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    margin: 25,
+    flex: 40,
+  },
+
+  chat_row: {
+    flexDirection: "row",
+    textAlign: "center",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    margin: 5,
+  },
+  make_event: {
+    marginBottom: 15,
+  },
+});
+
+const create_event = StyleSheet.create({
+  event_container: {
+    flexDirection: "column",
+    height: 500,
+    margin: 25,
+  },
+  title: {
+    flex: 3,
+  },
+  location: {
+    flex: 3,
+  },
+  date: {
+    flex: 3,
+  },
+  description: {
+    flex: 10,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
   },
 });
 
