ac65b05b011bcde3aadcbf69412d02619ccad153
kyulyeon <65508439+kyulyeon@users.noreply.github.com>
2022-05-03 18:54:44 -0700

    did backend for auth

--- router/authentication.js
+++ router/authentication.js
@@ -7,11 +7,11 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req,res)=> {
-
-const {email, password,primaryInterests,secondaryInterests} = req.body;
+// add first name and username
+const {email, password, firstName, userName, primaryInterests,secondaryInterests} = req.body;
 
 try {
-    const user = new User ({email, password,primaryInterests,secondaryInterests });
+    const user = new User ({email, password, firstName, userName,primaryInterests,secondaryInterests });
     await user.save();
     const token = jwt.sign({ userId: user._id}, 'MY_SECRET_KEY');
     res.send(token);
@@ -21,5 +21,31 @@ return res.status(422).send(err.message);
 
 
 });
+//add this too
+router.post('/signin', async (req,res)=> {
+    const {email, password } = req.body;
+    if (!email || !password){
+     return res.status(422).send({ error: 'Must provide email and passward'});
+    }
+   
+    const user = User.findOne({email, password});
+  //  console.log(user);
+    if(!user) {
+        return res.status(422).send({ error: 'email not found'});
+    }
+    try{
+        await user.comparePassword(password);
+ const token = jwt.sign({ userId: user._id, password: user.password}, 'MY_SECRET_KEY');
+    res.send(token);
+
+    }
+    catch (err){
+       // console.log(user);
+       // console.log(password);
+        console.log(err);
+        return res.status(422).send({ error: 'Must provide with password'});
+    }
+   });
+
 
 module.exports = router;
--- router/models/Users.js
+++ router/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema ({
 
@@ -12,6 +13,15 @@ const userSchema = new mongoose.Schema ({
         type: String,
         require: true
     },
+    // add firstName and lastName
+    firstName:{
+        type: String,
+        require: true
+    },
+    userName:{
+        type: String,
+        require: true
+    },
 
     primaryInterests:{
         type: String,
@@ -22,8 +32,45 @@ const userSchema = new mongoose.Schema ({
         type: String,
         require: true
     }
-
-
+    // move this here
 });
 
-mongoose.model('User', userSchema);
+    //add this 
+
+    userSchema.pre('save',function(next){
+        const user = this;
+        if(!user.isModified('password')){
+            return next();
+        }
+
+        bcrypt.genSalt(10, (err, salt) => {
+        
+        if (err){
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err){
+                return next(err)
+            }
+            user.password = hash;
+            next();
+        });
+        });
+    });
+    userSchema.methods.comparePassword = function(pass) {
+        return new Promise((resolve,reject) =>{ 
+            bcrypt.compare(pass,user.password, (err, isMatch) => {
+                if(err){
+                    return reject (err);
+                }
+                if (!isMatch){
+                    return reject(false);
+                }
+                resolve(true);
+            });
+
+        });
+    } 
+
+
+    module.exports = mongoose.model('User', userSchema);
