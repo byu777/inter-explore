a89a6cddab067a81e93f77651a0dca774e1f80f3
Gabriel Baluyut <60728482+gabeBaluyut@users.noreply.github.com>
2022-05-04 21:38:41 -0700

    Added navbar with icons and cleaned up format

--- landing page/landing_page.js
+++ landing page/landing_page.js
@@ -6,131 +6,176 @@ import {
   Image,
   ScrollView,
   Pressable,
-  Alert
+  Alert,
+  SafeAreaView,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 const menuOptions = () => {
   Alert.alert("Test", "This is a test");
-}
+};
 
 const LandingPage = () => {
   return (
-    <View style={styles.container}>
-      <ScrollView style={{marginHorizontal: 0}}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={{ marginHorizontal: 0 }}>
         {/* Implement call to DB to grab username later */}
         <View style={styles.importantSection}>
           <Text style={styles.welcomeText}>Welcome Gabriel!</Text>
         </View>
-        <Text style={{ color: "#023047", fontSize: 20, textAlign: "center"}}>
+        <Text style={{ color: "#023047", fontSize: 20, textAlign: "center" }}>
           Anything you want to share?
         </Text>
         <TextInput style={styles.inputArea} placeholder="Interest"></TextInput>
         <StatusBar style="auto" />
-        <Text style={{textAlign: "left", fontSize: 40, color: '#023047'}}>Feed</Text>
         <View style={styles.verticalItems}>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 40,
+              color: "#023047",
+              padding: 10,
+            }}
+          >
+            Feed
+          </Text>
+
           <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>j_payne likes Karaoke!</Text>
-          <Text>Justin Payne</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>j_payne likes Karaoke!</Text>
+              <Text>Justin Payne</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
 
           <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>eric_kwon likes fencing!</Text>
-          <Text>Eric Kwon</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
-          </View>
-
-          <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>rich_mac likes snowboarding!</Text>
-          <Text>Richard Mac</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>eric_kwon likes fencing!</Text>
+              <Text>Eric Kwon</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
 
           <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>Gab_baluyut likes ice hockey!</Text>
-          <Text>Gabriel Baluyut</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
-          </View>
-
-          <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>Gab_baluyut likes ice hockey!</Text>
-          <Text>Gabriel Baluyut</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>rich_mac likes snowboarding!</Text>
+              <Text>Richard Mac</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
 
           <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>Gab_baluyut likes ice hockey!</Text>
-          <Text>Gabriel Baluyut</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
-          </View>
-
-          <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>Gab_baluyut likes ice hockey!</Text>
-          <Text>Gabriel Baluyut</Text>
-          </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>Gab_baluyut likes ice hockey!</Text>
+              <Text>Gabriel Baluyut</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
 
           <View style={styles.activityItems}>
-          <Image source={require("../assets/80w/Icon80.png")}></Image>
-          <View style={styles.verticalItems}>
-          <Text>Gab_baluyut likes ice hockey!</Text>
-          <Text>Gabriel Baluyut</Text>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>Gab_baluyut likes ice hockey!</Text>
+              <Text>Gabriel Baluyut</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
-          <Pressable onPress={menuOptions}>
-          <Text style={{fontSize: 40}}>...</Text>
-          </Pressable>
+
+          <View style={styles.activityItems}>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>Gab_baluyut likes ice hockey!</Text>
+              <Text>Gabriel Baluyut</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
+          </View>
+
+          <View style={styles.activityItems}>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>Gab_baluyut likes ice hockey!</Text>
+              <Text>Gabriel Baluyut</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
+          </View>
+
+          <View style={styles.activityItems}>
+            <Image source={require("../assets/80w/Icon80.png")}></Image>
+            <View style={styles.verticalItems}>
+              <Text>Gab_baluyut likes ice hockey!</Text>
+              <Text>Gabriel Baluyut</Text>
+            </View>
+            <Pressable onPress={menuOptions}>
+              <Text style={{ fontSize: 40 }}>...</Text>
+            </Pressable>
           </View>
         </View>
-        
       </ScrollView>
       <View style={styles.navbarContainer}>
-          <View style={styles.navbar}>
-            <Pressable onPress={() => Alert.alert("Item pressed", "You pressed this button")} style={styles.iconBehave}>
-            <Image source={require("../assets/87w/Icon87.png")}></Image>
-            </Pressable>
-          </View>  
+        <View style={styles.navbar}>
+          <Pressable
+            onPress={() =>
+              Alert.alert("Item pressed", "You pressed this button")
+            }
+            style={styles.iconBehave}
+          >
+            <Image
+              source={require("../assets/baseline_home_black_24dp.png")}
+            ></Image>
+          </Pressable>
+          <Pressable
+            onPress={() =>
+              Alert.alert("Item pressed", "You pressed this button")
+            }
+            style={styles.iconBehave}
+          >
+            <Image
+              source={require("../assets/baseline_search_black_24dp.png")}
+            ></Image>
+          </Pressable>
+          <Pressable
+            onPress={() =>
+              Alert.alert("Item pressed", "You pressed this button")
+            }
+            style={styles.iconBehave}
+          >
+            <Image
+              source={require("../assets/baseline_chat_black_24dp.png")}
+            ></Image>
+          </Pressable>
+          <Pressable
+            onPress={() =>
+              Alert.alert("Item pressed", "You pressed this button")
+            }
+            style={styles.iconBehave}
+          >
+            <Image
+              source={require("../assets/baseline_person_black_24dp.png")}
+            ></Image>
+          </Pressable>
         </View>
-    </View>
+      </View>
+    </SafeAreaView>
   );
 };
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -143,23 +188,22 @@ const styles = StyleSheet.create({
   welcomeText: {
     color: "#023047",
     fontSize: 40,
-    width: '100%',
-    textAlign: "center"
+    width: "100%",
+    textAlign: "center",
   },
   importantSection: {
-    backgroundColor: "#ffb703",
     flex: 0,
     flexDirection: "row",
   },
   inputArea: {
     margin: 10,
     padding: 8,
-    width: '90%',
+    width: "90%",
     height: 50,
     borderWidth: 1,
     borderColor: "#023047",
     borderRadius: 10,
-    alignSelf: "center"
+    alignSelf: "center",
   },
   navbarContainer: {
     position: "absolute",
@@ -169,10 +213,10 @@ const styles = StyleSheet.create({
   navbar: {
     flexDirection: "row",
     backgroundColor: "#023047",
-    width: '90%',
+    width: "90%",
     justifyContent: "space-evenly",
     borderRadius: 20,
-    margin: 20
+    margin: 20,
   },
   iconBehave: {
     padding: 14,
@@ -181,13 +225,16 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
+    padding: 5,
   },
   verticalItems: {
     flex: 1,
     flexDirection: "column",
     flexWrap: "wrap",
-    margin: 10
-  }
+    margin: 10,
+    backgroundColor: "#FFB703",
+    borderRadius: 20,
+  },
 });
 
 export default LandingPage;
