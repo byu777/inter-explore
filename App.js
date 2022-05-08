eacb0dcc2a85c8c2e758968a794beffaec13d2aa
Johnny Ryue <johnryue1993@gmail.com>
2022-05-07 17:16:03 -0700

    merge conflicts for this branch

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import Profile from "./screens/Profile";
 import RootStack from "./screens/RootStack";
 import React from 'react';
 import Tabs from "./tabs/tab";
+import { View, Button, SafeAreaView, BrowserRouter } from 'react-native';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+//import { AppContext, socket } from './context/appContext';
 
 export default function App() {
   return (
