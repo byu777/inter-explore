6784d1d9b011c89176813e8d3977aeb7ad7fa7d4
Richard Mac <rmac2@my.bcit.ca>
2022-05-11 23:59:55 -0700

    implemented profile picture upload and displaying user information from database

--- api/tracker.js
+++ api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://8364-2001-569-744a-9c00-91fc-9d-78df-af49.ngrok.io'
+    baseURL: 'http://d79b-2001-569-744a-9c00-7c08-b4ea-5c44-5ef9.ngrok.io'
 })
