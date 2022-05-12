9f4a1bd178b0f580e617ba4d96fed4824b338937
Justin Payne <justinthecore@gmail.com>
2022-05-12 11:36:35 -0700

    SAVE STATE

--- api/tracker.js
+++ api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://cffe-2001-569-7ef4-6100-bd0a-7afa-2121-922.ngrok.io/'
+    baseURL: 'http://9c18-2001-569-7ef4-6100-a95f-9f1f-de4b-6a8f.ngrok.io/'
 })
