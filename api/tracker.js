c90dfae2fd572ba4f6e56d48059e88e8901ae6da
Richard Mac <rmac2@my.bcit.ca>
2022-05-12 11:05:55 -0700

    fixed bugs

--- api/tracker.js
+++ api/tracker.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
     baseURL: 'http://24f6-2001-569-7e2a-7c00-e10c-db49-5f23-71d.ngrok.io'
-=======
-    baseURL: 'http://d79b-2001-569-744a-9c00-7c08-b4ea-5c44-5ef9.ngrok.io'
->>>>>>> Richard
 })
