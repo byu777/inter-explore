fb54a406bef3e3754fc6c7a42073ceadea4f3846
Johnny Ryue <johnryue1993@gmail.com>
2022-05-12 11:09:49 -0700

    testing new dev change

--- api/tracker.js
+++ api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://24f6-2001-569-7e2a-7c00-e10c-db49-5f23-71d.ngrok.io'
+    baseURL: 'http://228d-2001-569-7e2a-7c00-4971-2a09-2e4c-2d77.ngrok.io'
 })
