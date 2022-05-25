8a8e654a62866e751186eaa6bb1e68f9bd39c077
Justin Payne <justinthecore@gmail.com>
2022-05-25 00:36:10 -0700

    changes

--- api/tracker.js
+++ api/tracker.js
@@ -3,5 +3,5 @@ import axios from "axios";
 // Forwarding URL from ngrok to allow other users access to your localhost
 // Add ngrok url to .env file (not sure how to access .env outside router.js)
 export default axios.create({
-    baseURL: process.env.NGROK_URL
+    baseURL: 'http://81bc-2001-569-7ef4-6100-2da0-f572-f062-34aa.ngrok.io/'
 })
